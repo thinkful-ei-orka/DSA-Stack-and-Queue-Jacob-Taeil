67cef519920d24f71d575cde6f09dba91366c4c9
JZPoteat <jzpoteat@gmail.com>
2020-07-17 15:10:01 -0400

    finished dllqueue.  working on queue stack implementation

--- dllQueue.js
+++ dllQueue.js
@@ -2,21 +2,22 @@ class _Node {
     constructor(value) {
         this.value = value;
         this.next = null;
+        this.previous = null;
     }
 }
-
-class Queue {
+//  1 - 2 - 3 - 4 
+class dllQueue {
     constructor() {
         this.first = null;
         this.last = null;
     }
     enqueue(data) {
         if (this.last === null) {
-            this.last = new _Node(data, null);
+            this.last = new _Node(data, null, null);
             this.first = this.last;
         } else {
             let lastNode = this.last;
-            this.last = new _Node(data, null);
+            this.last = new _Node(data, null, lastNode);
             lastNode.next = this.last;
         }
     }
@@ -26,6 +27,7 @@ class Queue {
         } else {
             let firstNode = this.first;
             this.first = this.first.next;
+            this.first.previous = null;
             return firstNode.value;
         }
     }
@@ -53,17 +55,17 @@ function display(queue) {
 }
 
 function main() {
-    let starTrekQ = new Queue;
-    starTrekQ.enqueue('Kirk');
-    starTrekQ.enqueue('Spock');
-    starTrekQ.enqueue('Uhura');
-    starTrekQ.enqueue('Sulu');
-    starTrekQ.enqueue('Checkov');
+    let DstarTrekQ = new dllQueue;
+    DstarTrekQ.enqueue('Kirk');
+    DstarTrekQ.enqueue('Spock');
+    DstarTrekQ.enqueue('Uhura');
+    DstarTrekQ.enqueue('Sulu');
+    DstarTrekQ.enqueue('Checkov');
 
-    starTrekQ.dequeue();
-    starTrekQ.dequeue();
+    DstarTrekQ.dequeue();
+    DstarTrekQ.dequeue();
 
-    console.log(display(starTrekQ));
+    console.log(display(DstarTrekQ));
 }
 
 main();
--- stack.js
+++ stack.js
@@ -24,6 +24,49 @@ class Stack {
     }
 }
 
+1 - 3 - 5;
+
+1 - 3 - 5; 
+
+5; 
+
+class stackQueue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+    }
+    enqueue(data) {
+        if(this.last === null) {
+            this.first = new Stack;
+            this.last = new Stack;
+            this.first.push(data);
+            this.last.push(data);
+        } else {
+            this.last.push(data);
+            this.first = this.alterFirst(this.first, data);
+        }
+    }
+    alterFirst(stack, newData) {
+        let oldStack = [];
+        let newStack = new Stack;
+        while(stack.top !== null) {
+            oldStack.push(stack.top.value);
+            stack.pop();
+        }
+        newStack.push(newData);
+        for (let i = (oldStack.length - 1); i > 0; i--) {
+            newStack.push(oldStack[i]);
+        }
+        return newStack;
+    }
+}
+
+function displayStackQueue(queue) {
+    let stack = queue.last;
+    console.log(queue);
+    display(stack);
+}
+
 function peek(stack) {
     return stack.top;
 }
@@ -34,6 +77,7 @@ function isEmpty(stack) {
     return false;
 }
 function display(stack) {
+    console.log(stack);
     let display = [];
     let node = stack.top;
     while (node !== null) {
@@ -59,41 +103,41 @@ function is_palindrome(s) {
 }
 
 function matching_parentheses(expression) {
-  let expStack = new Stack;
-  let expArr = expression.split('');
-  expArr.forEach(char => expStack.push(char));
+    let expStack = new Stack;
+    let expArr = expression.split('');
+    expArr.forEach(char => expStack.push(char));
 
-  let parCounter = 0;
-  let outOfOrderMax = 0;
+    let parCounter = 0;
+    let outOfOrderMax = 0;
 
-  // loop through the stack
-  while (expStack.top !== null) {
-    if (expStack.top.data === '(') {
-      parCounter -= 1;
-    } else if (expStack.top.data === ')') {
-      parCounter += 1;
+    // loop through the stack
+    while (expStack.top !== null) {
+        if (expStack.top.data === '(') {
+            parCounter -= 1;
+        } else if (expStack.top.data === ')') {
+            parCounter += 1;
+        }
+        if (parCounter < -outOfOrderMax) {
+            outOfOrderMax = -parCounter;
+        }
+        expStack.pop();
     }
-    if (parCounter < -outOfOrderMax) {
-      outOfOrderMax = -parCounter;
+
+    let message = '';
+    // set result messages
+    if (parCounter === 0) {
+        message = 'You have the same number of "(" as ")".';
+    } else if (parCounter < 0) {
+        message = `You have ${-parCounter} too many "("s.`;
+    } else if (parCounter > 0) {
+        message = `You have ${parCounter} too many ")"s.`;
     }
-    expStack.pop();
-  }
 
-  let message = '';
-  // set result messages
-  if (parCounter === 0) {
-    message = 'You have the same number of "(" as ")".';
-  } else if (parCounter < 0) {
-    message = `You have ${-parCounter} too many "("s.`;
-  } else if (parCounter > 0) {
-    message = `You have ${parCounter} too many ")"s.`;
-  }
+    if (outOfOrderMax) {
+        message += ` Your parentheses went out of order ${outOfOrderMax} times.`;
+    }
 
-  if (outOfOrderMax) {
-    message += ` Your parentheses went out of order ${outOfOrderMax} times.`;
-  }
-
-  return message;
+    return message;
 
 
     // let parCount = {
@@ -125,78 +169,78 @@ function matching_parentheses(expression) {
 }
 
 function sortStack(stack) {
-  let newStack = new Stack;
-  let largest = null;
-  let prevLargest = null;
-  let counter = 1;
+    let newStack = new Stack;
+    let largest = null;
+    let prevLargest = null;
+    let counter = 1;
 
-  // find the length of the stack
-  let length = 0;
-  let node = stack.top;
-  while (node !== null) {
-    length++;
-    node = node.next;
-  }
-
-  // loop through the stack
-  for (let i = 0; i < length; i++) {
-    largest = null;
-    counter = 0;
+    // find the length of the stack
+    let length = 0;
     let node = stack.top;
-    while (node !== null) { //  (first time, prevLargest = null)
-      if (prevLargest === null) {
-        if (largest === null) {
-          largest = node.data;
-          counter = 1;
-        }
-        if (node.data > largest) {
-          largest = node.data;
-          counter = 1;
-        }
-        if (node.data === largest) {
-          counter += 1;
-        }
-      } else { // prevLargest = 7;
-        if (node.data < prevLargest && largest === null) {
-          largest = node.data;
-          counter = 1;
-        }
-        if (node.data < prevLargest && node.data > largest) {
-          largest = node.data;
-          counter = 1;
-        }
-        if (node.data === largest) {
-          counter += 1;
-        }
-      }
-      // if (prevLargest === null) {
-      //   largest = node.data;
-      //   counter = 0;
-      // }
-      // if (largest === null) { // largest = 4
-      //   largest = node.data;
-      //   counter = 0;
-      // }
-      // if (prevLargest === null) {
-      //   if (node.data > largest) {
-      //     largest = node.data;
-      //     counter = 0;
-      //   }
-      // } else {
-      //   if (node.data > largest && node.data < prevLargest) {
-      //     largest = node.data;
-      //     counter = 0;
-      //   }
-      // }
-      node = node.next;
+    while (node !== null) {
+        length++;
+        node = node.next;
     }
-    for (let j = 0; j < counter; j++) {
-      newStack.push(largest);
-    }
-    prevLargest = largest; // prev largest = 3
-  }
 
-  return newStack;
+    // loop through the stack
+    for (let i = 0; i < length; i++) {
+        largest = null;
+        counter = 0;
+        let node = stack.top;
+        while (node !== null) { //  (first time, prevLargest = null)
+            if (prevLargest === null) {
+                if (largest === null) {
+                    largest = node.data;
+                    counter = 1;
+                }
+                if (node.data > largest) {
+                    largest = node.data;
+                    counter = 1;
+                }
+                if (node.data === largest) {
+                    counter += 1;
+                }
+            } else { // prevLargest = 7;
+                if (node.data < prevLargest && largest === null) {
+                    largest = node.data;
+                    counter = 1;
+                }
+                if (node.data < prevLargest && node.data > largest) {
+                    largest = node.data;
+                    counter = 1;
+                }
+                if (node.data === largest) {
+                    counter += 1;
+                }
+            }
+            // if (prevLargest === null) {
+            //   largest = node.data;
+            //   counter = 0;
+            // }
+            // if (largest === null) { // largest = 4
+            //   largest = node.data;
+            //   counter = 0;
+            // }
+            // if (prevLargest === null) {
+            //   if (node.data > largest) {
+            //     largest = node.data;
+            //     counter = 0;
+            //   }
+            // } else {
+            //   if (node.data > largest && node.data < prevLargest) {
+            //     largest = node.data;
+            //     counter = 0;
+            //   }
+            // }
+            node = node.next;
+        }
+        for (let j = 0; j < counter; j++) {
+            newStack.push(largest);
+        }
+        prevLargest = largest; // prev largest = 3
+    }
+
+    return newStack;
 }
 
 function main() {
@@ -212,23 +256,29 @@ function main() {
     starTrek.push('Scotty');
 
 
-  // console.log(display(starTrek));
-  // console.log(is_palindrome('racecar'));
-  // console.log(matching_parentheses('()awegaag(aawe)a()'));
-  // console.log(matching_parentheses('(ae(g)dsawse)))sd'));
-  // console.log(matching_parentheses('(((as)v(    (daasd)'));
-  // console.log(matching_parentheses('))fq)ad((qqwfqw('));
+    // console.log(display(starTrek));
+    // console.log(is_palindrome('racecar'));
+    // console.log(matching_parentheses('()awegaag(aawe)a()'));
+    // console.log(matching_parentheses('(ae(g)dsawse)))sd'));
+    // console.log(matching_parentheses('(((as)v(    (daasd)'));
+    // console.log(matching_parentheses('))fq)ad((qqwfqw('));
 
-  let unsorted = new Stack;
-  unsorted.push(4);
-  unsorted.push(7);
-  unsorted.push(3);
-  unsorted.push(4);
-  unsorted.push(6);
-  unsorted.push(4);
-  unsorted.push(1);
-  console.log(display(unsorted));
-  console.log(display(sortStack(unsorted)));
+    // let unsorted = new Stack;
+    // unsorted.push(4);
+    // unsorted.push(7);
+    // unsorted.push(3);
+    // unsorted.push(4);
+    // unsorted.push(6);
+    // unsorted.push(4);
+    // unsorted.push(1);
+    // console.log(display(unsorted));
+    // console.log(display(sortStack(unsorted)));
+
+    let stkQueue = new stackQueue;
+    stkQueue.enqueue(3);
+    stkQueue.enqueue(5);
+    stkQueue.enqueue(1);
+    console.log(displayStackQueue(stkQueue));
 }
 
 main();
